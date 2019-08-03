24d24dec60ba2b28e153d97d563e850912b2384f
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-03 03:17:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
+  array = array.sort
   return array.last
 end
 
 def find_min_value(array)
-  array.sort
+  array = array.sort
   return array.first
 end
